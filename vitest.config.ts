a52633d9d0c76b35f7dc6062cc0d1e84f7b910a9
justYu2001 <jack4781012@gmail.com>
2023-02-02 20:04:37 +0800

    chore: add configuration of vitest-fixture

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+import { resolve } from "path";
+
+export default defineConfig({
+    test: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+        },
+        deps: {
+            inline: ["vitest-fixture"],
+        },
+        testTimeout: 60_000,
+    },
+});
